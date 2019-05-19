2db1948dd72d6210aacef7118b236fa9f1103dc8
서재훈 <48151382+Jaehooooooon@users.noreply.github.com>
2019-05-19 17:02:40 +0900

    MainActivity

--- app/src/main/java/com/example/bars/MainActivity.java
+++ app/src/main/java/com/example/bars/MainActivity.java
@@ -1,13 +1,35 @@
-package com.example.bars;
+package org.sajae.myapplication8;
+//package com.example.bars;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MAIN_CAMERA_REQUESTCODE = 101;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        }
+    Button buttonCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonCamera = findViewById(R.id.button_camera);
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCameraActivity();
+            }
+        });
+    }
+
+    public void showCameraActivity() {
+        Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+        startActivityForResult(intent, MAIN_CAMERA_REQUESTCODE);
+    }
+
 }
