3a93b93374ec913401779d864bde35325b0f1661
HyeonJe <33957389+HyeonJe@users.noreply.github.com>
2019-05-21 23:47:38 +0900

    Merge branch 'master' of C:\Users\HHJ\AndroidStudioProjects\BARS with conflicts.

--- app/src/main/java/com/example/bars/CameraActivity.java
+++ app/src/main/java/com/example/bars/CameraActivity.java
@@ -1,13 +1,164 @@
 package com.example.bars;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class CameraActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView imgMain;
+    private Button btnConfirm, btnCancel;
+
+    private static final int PICK_FROM_CAMERA = 1;
+    private static final int CROP_FROM_CAMERA = 2;
+
+    private Uri photoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        imgMain = (ImageView)findViewById(R.id.img_test);
+        btnConfirm = (Button)findViewById(R.id.btn_confirm);
+        btnCancel = (Button)findViewById(R.id.btn_cancel);
+
+        takePhoto();
+    }
+
+    private void initView() {
+        imgMain = (ImageView)findViewById(R.id.img_test);
+        btnConfirm = (Button)findViewById(R.id.btn_confirm);
+        btnCancel = (Button)findViewById(R.id.btn_cancel);
+
+        btnConfirm.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_confirm:
+                // 확인 눌렀을 경우
+                break;
+            case R.id.btn_cancel:
+                // 취소 눌렀을 경우
+                break;
+        }
+    }
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException e) {
+            Toast.makeText(CameraActivity.this, "이미지 처리 오류! 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+            finish();
+            e.printStackTrace();
+        }
+        if (photoFile != null) {
+            photoUri = FileProvider.getUriForFile(CameraActivity.this,
+                    "com.example.bars.provider", photoFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            startActivityForResult(intent, PICK_FROM_CAMERA);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("HHmmss").format(new Date());
+        String imageFileName = "nostest_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/NOSTest/");
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (requestCode == PICK_FROM_CAMERA) {
+            cropImage();
+            // 갤러리에 나타나게
+            MediaScannerConnection.scanFile(CameraActivity.this,
+                    new String[]{photoUri.getPath()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                        }
+                    });
+        } else if (requestCode == CROP_FROM_CAMERA) {
+            imgMain.setImageURI(null);
+            imgMain.setImageURI(photoUri);
+            revokeUriPermission(photoUri, Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
+    }
+
+    public void cropImage() {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(photoUri, "image/*");
+        List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, 0);
+        grantUriPermission(list.get(0).activityInfo.packageName, photoUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        int size = list.size();
+        if (size == 0) {
+            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            Toast.makeText(this, "용량이 큰 사진의 경우 시간이 오래 걸릴 수 있습니다.", Toast.LENGTH_SHORT).show();
+            intent.putExtra("crop", "true");
+
+            intent.putExtra("aspectX", 1);
+            intent.putExtra("aspectY", 1);
+            intent.putExtra("scale", true);
+
+            File croppedFileName = null;
+            try {
+                croppedFileName = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            File folder = new File(Environment.getExternalStorageDirectory() + "/NOSTest/");
+            File tempFile = new File(folder.toString(), croppedFileName.getName());
+            photoUri = FileProvider.getUriForFile(CameraActivity.this,
+                    "com.example.bars.provider", tempFile);
+            intent.putExtra("return-data", false);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+
+            Intent i = new Intent(intent);
+            ResolveInfo res = list.get(0);
+            grantUriPermission(res.activityInfo.packageName, photoUri, Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            i.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            startActivityForResult(i, CROP_FROM_CAMERA);
+
+        }
     }
 }
