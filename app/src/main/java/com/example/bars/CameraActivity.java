c02455526e7977e3256ea84bcf82f79ec56a780e
HyeonJe <33957389+HyeonJe@users.noreply.github.com>
2019-05-21 23:52:23 +0900

    update CameraActivity put Permission on MainActivity

--- app/src/main/java/com/example/bars/CameraActivity.java
+++ app/src/main/java/com/example/bars/CameraActivity.java
@@ -1,22 +1,16 @@
 package com.example.bars;
 
-import android.Manifest;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -25,7 +19,6 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
--- app/src/main/java/com/example/bars/MainActivity.java
+++ app/src/main/java/com/example/bars/MainActivity.java
@@ -41,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showCameraActivity() {
-        Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-        intent.putExtra("id", "hite");  //예시를 위해 main에서 바로 result로 넘겼습니다. (intent 담아서)
+        Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+        //intent.putExtra("id", "hite");  //예시를 위해 main에서 바로 result로 넘겼습니다. (intent 담아서)
         startActivityForResult(intent, MAIN_CAMERA_REQUESTCODE);
     }
 
