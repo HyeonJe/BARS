b3832b11789f30ba6f04027619a65cb94031b767
HyeonJe <33957389+HyeonJe@users.noreply.github.com>
2019-05-23 23:09:25 +0900

    인식 모듈 추가 및 카메라, 컨펌 액티비티 수정 build.gradle 수정 Manifest 수정

--- app/src/main/java/com/example/bars/CameraActivity.java
+++ app/src/main/java/com/example/bars/CameraActivity.java
@@ -1,160 +1,66 @@
 package com.example.bars;
 
-import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.Intent;
-import android.content.pm.ResolveInfo;
-import android.graphics.Bitmap;
-import android.media.MediaScannerConnection;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.os.Bundle;
+
+
+import com.soundcloud.android.crop.Crop;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 
-public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
-    private ImageView imgMain;
-    private Button btnConfirm, btnCancel;
-
-    private static final int PICK_FROM_CAMERA = 1;
-    private static final int CROP_FROM_CAMERA = 2;
-
+public class CameraActivity extends AppCompatActivity {
+    public static final int REQUEST_IMAGE = 100;
     private Uri photoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-
-        imgMain = (ImageView)findViewById(R.id.img_test);
-        btnConfirm = (Button)findViewById(R.id.btn_confirm);
-        btnCancel = (Button)findViewById(R.id.btn_cancel);
-
+        /* take Photo of beer */
         takePhoto();
     }
 
-    private void initView() {
-        imgMain = (ImageView)findViewById(R.id.img_test);
-        btnConfirm = (Button)findViewById(R.id.btn_confirm);
-        btnCancel = (Button)findViewById(R.id.btn_cancel);
-
-        btnConfirm.setOnClickListener(this);
-        btnCancel.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_confirm:
-                // 확인 눌렀을 경우
-                break;
-            case R.id.btn_cancel:
-                finish();
-                break;
-        }
-    }
     private void takePhoto() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        // tell camera where to store the resulting picture
+        photoUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File photoFile = null;
-        try {
-            photoFile = createImageFile();
-        } catch (IOException e) {
-            Toast.makeText(CameraActivity.this, "이미지 처리 오류! 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
-            finish();
-            e.printStackTrace();
-        }
-        if (photoFile != null) {
-            photoUri = FileProvider.getUriForFile(CameraActivity.this,
-                    "com.example.bars.provider", photoFile);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-            startActivityForResult(intent, PICK_FROM_CAMERA);
-        }
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // start camera, and wait for it to finish
+        startActivityForResult(intent, REQUEST_IMAGE);
     }
 
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("HHmmss").format(new Date());
-        String imageFileName = "nostest_" + timeStamp + "_";
-        File storageDir = new File(Environment.getExternalStorageDirectory() + "/NOSTest/");
-        if (!storageDir.exists()) {
-            storageDir.mkdirs();
-        }
-        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
-
-        return image;
-    }
-
+    // dictates what to do after the user takes an image, selects and image, or crops an image
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode != RESULT_OK) {
-            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-        if (requestCode == PICK_FROM_CAMERA) {
-            cropImage();
-            // 갤러리에 나타나게
-            MediaScannerConnection.scanFile(CameraActivity.this,
-                    new String[]{photoUri.getPath()}, null,
-                    new MediaScannerConnection.OnScanCompletedListener() {
-                        public void onScanCompleted(String path, Uri uri) {
-                        }
-                    });
-        } else if (requestCode == CROP_FROM_CAMERA) {
-            imgMain.setImageURI(null);
-            imgMain.setImageURI(photoUri);
-            revokeUriPermission(photoUri, Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-
-        }
-    }
-
-    public void cropImage() {
-        Intent intent = new Intent("com.android.camera.action.CROP");
-        intent.setDataAndType(photoUri, "image/*");
-        List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, 0);
-        grantUriPermission(list.get(0).activityInfo.packageName, photoUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        int size = list.size();
-        if (size == 0) {
-            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        } else {
-            Toast.makeText(this, "용량이 큰 사진의 경우 시간이 오래 걸릴 수 있습니다.", Toast.LENGTH_SHORT).show();
-            intent.putExtra("crop", "true");
-
-            intent.putExtra("aspectX", 1);
-            intent.putExtra("aspectY", 1);
-            intent.putExtra("scale", true);
-
-            File croppedFileName = null;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        // if the camera activity is finished, obtained the uri, crop it to make it square, and send it to 'Classify' activity
+        if(requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
             try {
-                croppedFileName = createImageFile();
-            } catch (IOException e) {
+                Uri source_uri = photoUri;
+                Uri dest_uri = Uri.fromFile(new File(getCacheDir(), "cropped"));
+                // need to crop it to square image as CNN's always required square input
+                Crop.of(source_uri, dest_uri).asSquare().start(CameraActivity.this);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            File folder = new File(Environment.getExternalStorageDirectory() + "/NOSTest/");
-            File tempFile = new File(folder.toString(), croppedFileName.getName());
-            photoUri = FileProvider.getUriForFile(CameraActivity.this,
-                    "com.example.bars.provider", tempFile);
-            intent.putExtra("return-data", false);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
-
-            Intent i = new Intent(intent);
-            ResolveInfo res = list.get(0);
-            grantUriPermission(res.activityInfo.packageName, photoUri, Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-            i.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
-            startActivityForResult(i, CROP_FROM_CAMERA);
-
+        }
+        // if cropping acitivty is finished, get the resulting cropped image uri and send it to 'Classify' activity
+        else if(requestCode == Crop.REQUEST_CROP && resultCode == RESULT_OK){
+            photoUri = Crop.getOutput(data);
+            Intent i = new Intent(CameraActivity.this, ConfirmActivity.class);
+            // put image data in extras to send
+            i.putExtra("resID_uri", photoUri);
+            startActivity(i);
         }
     }
 }
